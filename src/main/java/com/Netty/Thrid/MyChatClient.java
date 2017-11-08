51c84dc9b34d6c9cf99149bd668fe247425c291e
zqy <249449726@qq.com>
2017-11-08 23:39:21 +0800

    commit

--- src/main/java/com/Netty/Thrid/MyChatClient.java
+++ src/main/java/com/Netty/Thrid/MyChatClient.java
@@ -1,22 +1,30 @@
-package com.Netty.second;
+package com.Netty.Thrid;
 
+import com.Netty.second.MyServerInitializer;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
-public class MyClient {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MyChatClient {
     public static void main(String[] args) throws Exception{
         EventLoopGroup eventLoopGroup =new NioEventLoopGroup();
 
         try{
             Bootstrap bootstrap = new Bootstrap();
             bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class).
-                    handler(new MyServerInitializer());
+                    handler(new MyChatClientinitializer());
 
-            ChannelFuture channelFuture = bootstrap.connect("localhost",8899).sync();
-            channelFuture.channel().closeFuture().sync();
+            Channel channel = bootstrap.connect("localhost",8899).sync().channel();
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            for(;;){
+              channel.writeAndFlush(br.readLine()+"/r/n");
+            }
         }finally {
             eventLoopGroup.shutdownGracefully();
 
--- src/main/java/com/Netty/first/TestServerinitalizer.java
+++ src/main/java/com/Netty/first/TestServerinitalizer.java
@@ -1,12 +1,13 @@
 package com.Netty.first;
 
+
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpServerCodec;
 
 
-public class TestServerinitalizer extends ChannelInitializer<SocketChannel>{
+public class TestServerinitalizer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();//管道
